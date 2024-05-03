22fd180e721ce0b311fc5cbca981ad2528434234
김재정 <137398992+verisign90@users.noreply.github.com>
2024-05-03 17:57:38 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int sum = 0;
+        
+        for(int i=0; i<numbers.length; i++) {
+            sum += numbers[i];
+            if(sum > n) {
+                break;
+            }
+        }
+        return sum;
+    }
+}
