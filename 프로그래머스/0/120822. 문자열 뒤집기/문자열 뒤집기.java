34b8a3f3d69b0e9170dc2afb6d0c6b61fbee1890
김재정 <137398992+verisign90@users.noreply.github.com>
2024-05-10 20:11:53 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        // StringBuffer str = new StringBuffer(my_string);
+        // return str.reverse().toString();
+        return new StringBuffer(my_string).reverse().toString();
+    }
+}
