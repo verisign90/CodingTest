6cbc78f1be30e60580d0878fa3f7d46c3bdaa092
김재정 <137398992+verisign90@users.noreply.github.com>
2024-05-27 07:03:51 +0900

    [level 2] Title: 조건에 맞는 아이템들의 가격의 총합 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273709. 조건에 맞는 아이템들의 가격의 총합 구하기/조건에 맞는 아이템들의 가격의 총합 구하기.sql
+++ 프로그래머스/2/273709. 조건에 맞는 아이템들의 가격의 총합 구하기/조건에 맞는 아이템들의 가격의 총합 구하기.sql
@@ -1,5 +1,3 @@
--- 코드를 작성해주세요
-select sum(price) as total_price
-from item_info
-group by rarity
-having rarity = 'LEGEND';
+SELECT SUM(PRICE) AS TOTAL_PRICE
+FROM ITEM_INFO
+WHERE RARITY = 'LEGEND';
