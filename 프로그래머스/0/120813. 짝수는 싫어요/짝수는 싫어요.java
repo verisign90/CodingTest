1122b3f40b7f0eacb8da72cd55af6b80b80f62b0
김재정 <137398992+verisign90@users.noreply.github.com>
2024-05-26 09:16:50 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=1; i<=n; i+=2) {
+            list.add(i);
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
