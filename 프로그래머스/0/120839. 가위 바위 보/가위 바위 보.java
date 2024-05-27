1f9290b35ed1503fcefd537c48aab2f65b33d921
김재정 <137398992+verisign90@users.noreply.github.com>
2024-05-27 17:15:27 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : rsp.toCharArray()) {
+            if(c == '2') {
+                sb.append('0');
+            } else if(c == '0') {
+                sb.append('5'); 
+            } else {
+                sb.append('2');
+            }
+        }
+        
+        return sb.toString();
+    }
+}
