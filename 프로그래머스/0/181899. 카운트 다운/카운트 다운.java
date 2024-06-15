0a210770bc5ef03e1aeb0959e79f2f1389cdd84f
김재정 <137398992+verisign90@users.noreply.github.com>
2024-06-15 11:40:22 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num - end_num + 1];
+        int count = start_num;
+        
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = count;
+            count--;
+        }
+        
+        return answer;
+    }
+}
