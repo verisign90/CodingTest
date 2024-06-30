4c7eb06d982770cacb6d0cf3087d9761dc66aeea
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-01 03:38:27 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int[] array, int height) {
-        int sum = 0;
+        int count = 0;
         for(int i=0; i<array.length; i++) {
             if(array[i] > height) {
-                //sum += 1;
-                sum++;
+                count++;
             }
         }
-        return sum;
+        
+        return count;
     }
 }
