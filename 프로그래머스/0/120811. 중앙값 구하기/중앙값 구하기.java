83018a315a6340ddeeea6b089990fd7589eb3447
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-01 03:38:39 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.47 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -3,6 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        return array[array.length/2];
+        return array[array.length / 2];
     }
 }
