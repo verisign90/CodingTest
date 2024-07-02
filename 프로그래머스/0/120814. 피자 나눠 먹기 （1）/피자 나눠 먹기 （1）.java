a2d666f8d618353fe2359abfcdc30094c9e2910f
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-02 21:34:56 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int n) {
-        if(n%7 == 0) {
-            return n/7;
+        if(n % 7 == 0) {
+            return n / 7;
         }
-        
-        return n/7 + 1;
+        return n / 7 + 1;
     }
 }
