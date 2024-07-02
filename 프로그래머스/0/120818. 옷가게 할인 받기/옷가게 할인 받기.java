df43296b30cac82507c9a2f597949cd71023cf60
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-02 21:35:18 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,21 +1,13 @@
 class Solution {
     public int solution(int price) {
-//         if(price >= 500000) {
-//             return (int)(price * 0.8);
-//         } else if(price >= 300000) {
-//             return (int)(price * 0.9);
-//         } else if(price >= 100000) {
-//             return (int)(price * 0.95);
-//         }
-//         return price;
-
         if(price >= 500000) {
-            price *= 0.8;
+            return (int)(price * 0.8);
         } else if(price >= 300000) {
-            price *= 0.9;
+            return (int)(price * 0.9);
         } else if(price >= 100000) {
-            price *= 0.95;
+            return (int)(price * 0.95);
+        } else {
+            return price;
         }
-        return price;
     }
 }
