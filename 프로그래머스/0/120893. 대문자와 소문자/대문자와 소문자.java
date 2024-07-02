649123ae3de14611dbddaad965b39d7f2d804779
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-02 21:36:28 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.72 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,37 +1,14 @@
 class Solution {
     public String solution(String my_string) {
-//         char[] array = my_string.toCharArray();
-        
-//         for(int i=0; i<array.length; i++) {
-//             if(Character.isUpperCase(array[i])) {
-//                 array[i] = Character.toLowerCase(array[i]);
-//             } else {
-//                 array[i] = Character.toUpperCase(array[i]);
-//             }
-//         }
-        
-//         return new String(array);
-        
-//         String answer = "";
-        
-//         for(int i=0; i<my_string.length(); i++) {
-//             if(Character.isUpperCase(my_string.charAt(i))) {
-//                 answer += Character.toLowerCase(my_string.charAt(i));
-//             } else {
-//                 answer += Character.toUpperCase(my_string.charAt(i));
-//             }
-//         }
-        
-//         return answer;
-        
         StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<my_string.length(); i++) {
             char ch = my_string.charAt(i);
-            if(Character.isUpperCase(ch)) {
-                sb.append(Character.toLowerCase(ch));
-            } else {
+            
+            if(Character.isLowerCase(ch)) {
                 sb.append(Character.toUpperCase(ch));
+            } else {
+                sb.append(Character.toLowerCase(ch));
             }
         }
         
