773e90ee0800d4f35358d804109e36f13240cfd5
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-02 21:36:43 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] solution(int[] array) {
-        int answer = 0;
+        int[] answer = new int[2];
+        int max = array[0];
         int index = 0;
-        
-        for(int i=0; i<array.length; i++) {
-            if(array[i] > answer) {
-                answer = array[i];
+        for(int i=1; i<array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
                 index = i;
             }
         }
         
-        int[] ans = new int[2];
-        ans[0] = answer;
-        ans[1] = index;
-        
-        return ans;
+        answer[0] = max;
+        answer[1] = index;
+        return answer;
     }
 }
