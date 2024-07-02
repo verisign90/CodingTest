5b3d79615d0e24149987f689094a36b667d57042
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-02 21:35:53 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,26 +1,12 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 class Solution {
     public int solution(String my_string) {
-//         int sum = 0;
-//         Pattern pattern = Pattern.compile("\\d");
-//         Matcher matcher = pattern.matcher(my_string);
-        
-//         while(matcher.find()) {
-//             sum += Integer.parseInt(matcher.group());
-//         }
-        
-//         return sum;
-        
         int sum = 0;
-        
-        //my_string = my_string.replaceAll("[a-zA-Z]", "");
-        my_string = my_string.replaceAll("[^0-9]", "");
-        String[] array = my_string.split("");
-        
-        for(String i : array) {
-            sum += Integer.parseInt(i);
+        for(int i=0; i<my_string.length(); i++) {
+            char ch = my_string.charAt(i);
+            
+            if(Character.isDigit(ch)) {
+                sum += Character.getNumericValue(ch);
+            }
         }
         
         return sum;
