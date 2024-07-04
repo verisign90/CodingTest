6db9ad9d2d5f7614ec22c43c8a2301a19eb54e2e
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-04 21:54:54 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,15 +1,4 @@
--- 코드를 입력하세요
--- SELECT name
--- from (select name 
---       from animal_ins
---       order by datetime)
--- where rownum = 1;      
-
--- select name
--- from animal_ins
--- where datetime = (select min(datetime) from animal_ins);
-
-select name
+SELECT name
 from animal_ins
 order by datetime
-fetch first 1 row only;
+limit 1;
