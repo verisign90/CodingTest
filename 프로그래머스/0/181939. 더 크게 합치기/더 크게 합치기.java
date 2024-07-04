5be9a85df0fcd274519e8e4e98c944ab32492df3
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-04 22:03:10 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.79 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+        
+        if(ab == ba) {
+            return ab;
+        } else {
+            return Math.max(ab, ba);
+        }
+    }
+}
