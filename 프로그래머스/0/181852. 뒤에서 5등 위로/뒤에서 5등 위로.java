b69d005d1ddd42bdcedc905f7106457082bad5b3
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-05 18:27:16 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.31 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -4,8 +4,5 @@ class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
         return Arrays.copyOfRange(num_list, 5, num_list.length);
-        
-        // num_list = Arrays.copyOfRange(num_list, 5, num_list.length);
-        // return num_list;
     }
 }
