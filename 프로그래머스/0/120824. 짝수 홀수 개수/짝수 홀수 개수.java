3a4416a06081d175e547d03a19c1af3f0609746b
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-10 01:06:02 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int a = 0;
-        int b = 0;
-        
-        for(int i=0; i<num_list.length; i++) {
-            if(num_list[i]%2 == 0) {
-                a++;
+        int odd = 0;
+        int even = 0;
+        for(int num : num_list) {
+            if(num % 2 == 0) {
+                even++;
             } else {
-                b++;
+                odd++;
             }
         }
         
-        return new int[]{a,b};
+//         int[] answer = new int[2];
+//         answer[0] = even;
+//         answer[1] = odd;
+        
+//         return answer;
+        return new int[]{even, odd};
     }
 }
