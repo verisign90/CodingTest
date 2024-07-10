20d8da9aabd4adc1c25cb302fd7e994dac84e454
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-10 19:30:18 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,40 +1,28 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-//     public List solution(int[] num_list, int n) {
-//         List<Integer> answer = new ArrayList<>();
-        
-//         for(int i=0; i<num_list.length; i+=n) {
-//             answer.add(num_list[i]);
-//         }
-        
-//         return answer;
-//     }
-    
     public int[] solution(int[] num_list, int n) {
-        int size = (int)Math.ceil((double)num_list.length / n);
-        int[] answer = new int[size];
-        int index = 0;
-        
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<num_list.length; i+=n) {
-            answer[index++] = num_list[i];    
+            list.add(num_list[i]);
         }
         
-        return answer;
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
         
-//         ArrayList<Integer> list = new ArrayList<>();
+//         int[] answer;
+//         if(num_list.length % n == 0) {
+//             answer = new int[num_list.length / n];
+//         } else {
+//             answer = new int[num_list.length / n + 1];
+//         }
         
+//         int index = 0;
 //         for(int i=0; i<num_list.length; i+=n) {
-//             list.add(num_list[i]);
+//             answer[index++] = num_list[i]; 
 //         }
-
-//         int[] answer = new int[list.size()];
-        
-//         for(int i=0; i<answer.length; i++) {
-//             answer[i] = list.get(i);
-//         }
-        
-//         return answer;
+        return answer;
     }
 }
