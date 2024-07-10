6de35bcb50cc7cad53e9af7205346222c9b7c318
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-10 21:56:53 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,25 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-//         int[] array1 = Arrays.copyOfRange(num_list, 0, n); //0부터 2까지. [5, 2, 1]
-//         int[] array2 = Arrays.copyOfRange(num_list, n, num_list.length); //3부터 4까지. [7, 5]
+//         int[] first = Arrays.copyOfRange(num_list, 0, n);   //2
+//         int[] second = Arrays.copyOfRange(num_list, n, num_list.length);    //1, 6
         
-//         int[] result = new int[num_list.length];  //num_list.length = 5, n=3
-//         System.arraycopy(array1, 0, result, array2.length, array1.length); //[0, 0, 5, 2, 1]
-//         System.arraycopy(array2, 0, result, 0, array2.length); //[7, 5, 5, 2, 1]
+//         int[] answer = new int[num_list.length];
         
-//         return result;
-    
+//         System.arraycopy(first, 0, answer, second.length, first.length);    //x, x, 2
+//         System.arraycopy(second, 0, answer, 0, second.length);
+//         return answer;
+        
         int[] answer = new int[num_list.length];
         int index = 0;
-        
-        for(int i=n; i<num_list.length; i++) {
-            answer[index++] = num_list[i];  //[0] = 7, [1] = 5
+        for(int i=n; i<num_list.length; i++) {  //1, 6
+            answer[index++] = num_list[i];  //[0]=1, [1]=6
         }
         
         for(int i=0; i<n; i++) {
-            answer[index++] = num_list[i];  //[2] = 5, [3] = 2, [4] = 1
+            answer[index++] = num_list[i];
         }
         
         return answer;
