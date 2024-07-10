b9b6f520bcc8d29f6372c045f2875dbc3ec3200d
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-10 17:52:38 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,52 +1,15 @@
 class Solution {
     public int solution(int[] num_list) {
-//         int answer;
-    
-//         if(num_list.length >= 11) {
-//             answer = 0;
-//             for(int i=0; i<num_list.length; i++) {
-//                 answer += num_list[i];
-//             }
-//         } else {
-//             answer = 1;
-//             for(int i=0; i<num_list.length; i++) {
-//                 answer *= num_list[i];
-//             }
-//         }
-        
-//         return answer;
-        
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-        
-//         int sum = 0;
-//         int mul = 1;
-//         int answer;
-        
-//         for(int i=0; i<num_list.length; i++) {
-//             if(num_list.length >= 11) {
-//                 sum += num_list[i];
-//                 answer = sum;
-//             } else {
-//                 mul *= num_list[i];
-//                 answer = mul;
-//             }
-//         }
-        
-//         return answer;
-        
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-        
         int sum = 0;
-        int mul = 1;
-        int answer;
-        
-        for(int i=0; i<num_list.length; i++) {
-            sum += num_list[i];
-            mul *= num_list[i];
+        int multi = 1;
+        for(int num : num_list) {
+            sum += num;
+            multi *= num;
         }
         
-        answer = (num_list.length >= 11) ? sum : mul;
-        
-        return answer;
+        if(num_list.length >= 11) {
+            return sum;
+        }
+        return multi;
     }
-}    
+}
