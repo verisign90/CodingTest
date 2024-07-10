2d90dc2e1c7f97d8ea14360f12eebee2189e75bd
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-11 03:05:24 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<names.length; i+=5) {
+            list.add(names[i]);
+        }
+        
+        String[] answer = new String[list.size()];
+        return list.toArray(answer);
+    }
+}
