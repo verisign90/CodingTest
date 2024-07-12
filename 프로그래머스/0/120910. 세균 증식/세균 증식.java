781b0de4e45593f06bdfa869836b235e88bbafc7
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-12 14:50:24 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n, int t) {
-        //return (int) Math.pow(2, t) * n;
+//         for(int i=0; i<t; i++) {
+//             n *= 2;
+//         }
+//         return n;
         
-        for(int i=0; i<t; i++) {
-            n *= 2;
-        }
+         //return n * (1 << t);
         
-        return n;
+        return (int)(n * Math.pow(2, t));
     }
 }
