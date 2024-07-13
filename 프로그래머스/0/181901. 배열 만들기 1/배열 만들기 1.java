cc74e93713b835b6cc8c77416c659366dc61e184
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-13 22:38:15 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.08 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,40 +1,17 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int k) {
-//         List<Integer> list = new ArrayList<>();
-        
-//         for(int i=1; i<=n; i++) {
-//             if(i % k == 0) {
-//                 list.add(i);
-//             }
-//         }
-        
-//         int[] answer = new int[list.size()];
-//         for(int i=0; i<list.size(); i++) {
-//             answer[i] = list.get(i);
-//         }
-        
-//         return answer;
-        
         int[] answer = new int[n/k];
+        int index = 0;
+        // for(int i=1; i<=n; i++) {
+        //     if(i % k == 0) {
+        //         answer[index++] = i;
+        //     }
+        // }
+        // return answer;
         
-        for(int i=1; k * i<=n; i++) {
-            answer[i-1] = k * i;
+        for(int i=1; i*k<=n; i++) {
+            answer[index++] = i*k;
         }
-        
         return answer;
-        
-//         int[] answer = new int[n/k];
-//         int index = 0;
-        
-//         for(int i=1; i<=n; i++) {
-//             if(i % k == 0) {
-//                 answer[index] = i;
-//                 index++;
-//             }
-//         }
-        
-//         return answer;
     }
 }
