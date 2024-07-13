e90061ca1399ba96975ba1991ec7e090b59bad39
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-13 23:43:38 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.06 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if(my_string.startsWith(is_prefix)) {
+            return 1;
+        }
+        return 0;
+    }
+}
