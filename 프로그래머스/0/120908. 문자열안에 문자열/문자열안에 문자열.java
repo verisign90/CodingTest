7e160d8b094eb2411a1bb61394285cccc6e25c65
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-15 19:13:27 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -2,22 +2,7 @@ class Solution {
     public int solution(String str1, String str2) {
         if(str1.contains(str2)) {
             return 1;
-        } else {
-            return 2;
         }
+        return 2;
     }
 }
-
-// class Solution {
-//     public int solution(String str1, String str2) {
-//         int answer = 0;
-        
-//         if(str1.contains(str2)) {
-//             answer = 1;
-//         } else {
-//             answer = 2;
-//         }
-        
-//         return answer;
-//     }
-// }
