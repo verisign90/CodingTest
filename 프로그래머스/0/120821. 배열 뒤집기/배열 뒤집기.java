674f70aba3ba0afa53c9c2955cdcc3600f3924c0
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-17 19:15:14 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,24 +1,24 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        for(int i=0; i<num_list.length / 2; i++) {
-            int temp = num_list[i];
-            num_list[i] = num_list[num_list.length-1-i];
-            num_list[num_list.length-1-i] = temp;
-        }
-        return num_list;
-
-        // int[] reverse = new int[num_list.length];
-        // for(int i=0; i<num_list.length; i++) {
-        //     reverse[i] = num_list[num_list.length-1-i];
-        // }
-        // return reverse;
+//         List<Integer> list = new ArrayList<>();
+//         for(int i=num_list.length-1; i>=0; i--) {
+//             list.add(num_list[i]);
+//         }
         
-        // List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
-        // Collections.reverse(list);
-        // return list.stream().mapToInt(Integer::intValue).toArray();
+//         int[] answer = new int[list.size()];
+//         for(int i=0; i<answer.length; i++) {
+//             answer[i] = list.get(i);
+//         }
+        
+//         return answer;
+        
+        int[] answer = new int[num_list.length];
+        int index = 0;
+        for(int i=num_list.length-1; i>=0; i--) {
+            answer[index++] = num_list[i];
+        }
+        return answer;
     }
 }
-
