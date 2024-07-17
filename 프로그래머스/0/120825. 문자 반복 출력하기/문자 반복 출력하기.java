bf65a8c8afa2cac0557afd2c602cedb8e5abbe93
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-18 02:48:23 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 7.66 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,27 +1,25 @@
 class Solution {
     public String solution(String my_string, int n) {
         StringBuilder sb = new StringBuilder();
+        // for(int i=0; i<my_string.length(); i++) {
+        //     for(int j=0; j<n; j++) {
+        //         sb.append(my_string.charAt(i));
+        //     }
+        // }
         
-//        for(int i=0; i<my_string.length(); i++) {
-//             char c = my_string.charAt(i);
-//             for(int j=0; j<n; j++) {
-//                 sb.append(c);
-//             }
-//         }
-        
-//         return sb.toString();
-        
-//         String[] arr = my_string.split("");
-        
-//         for(int i=0; i<arr.length; i++) {
-//             sb.append(arr[i].repeat(n));
-//         }
-        
-//         return sb.toString();
-        
-        for(char c : my_string.toCharArray()) {
-            sb.append((c + "").repeat(n));
+        for(int i=0; i<my_string.length(); i++) {
+            //sb.append(String.valueOf(my_string.charAt(i)).repeat(n));
+            sb.append(("" + my_string.charAt(i)).repeat(n));
         }
+        
+        // String[] answer = my_string.split("");
+        // for(int i=0; i<answer.length; i++) {
+        //     sb.append(answer[i].repeat(n));
+        // }
+        
+        // for(char c : my_string.toCharArray()) {
+        //     sb.append((c + "").repeat(n));
+        // }
         return sb.toString();
     }
 }
