0db6f299bf350aaace2f3496d30a60837ff5ba28
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-18 19:02:40 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 1;
-        for(int i=0; i<box.length; i++) {
-            answer *= box[i] / n;
-        }
-        
-        return answer;
+        return (box[0]/n) * (box[1]/n) * (box[2]/n);
     }
 }
