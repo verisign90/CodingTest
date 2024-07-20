e051ec93232238ebbb3eb57ceede32076be0c3fa
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-21 00:09:51 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,6 +1,7 @@
-select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, 
-       to_char(r.created_date, 'YYYY-MM-DD') as created_date
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, 
+       date_format(r.created_date, '%Y-%m-%d')
 from used_goods_board b
-join used_goods_reply r on b.board_id = r.board_id
-where to_char(b.created_date, 'YYYY-MM') = '2022-10'
+join used_goods_reply r
+on b.board_id = r.board_id
+where year(b.created_date) = 2022 and month(b.created_date) = 10
 order by r.created_date, b.title;
