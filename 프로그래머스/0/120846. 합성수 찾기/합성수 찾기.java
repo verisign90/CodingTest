363f1a76605cc16ebe4b5008c93ca288750a3b93
김재정 <137398992+verisign90@users.noreply.github.com>
2024-07-31 16:45:45 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++) {
+            int count = 0;
+            for(int j=1; j<=i; j++) {
+                if(i % j == 0) {
+                    count++;
+                    if(count == 3) {
+                        list.add(i);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return list.size();
+    }
+}
