5fd262ac5b00ceb20bb136d32556e48f3b8b2fcc
김재정 <137398992+verisign90@users.noreply.github.com>
2024-09-28 22:12:56 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -0,0 +1,11 @@
+function solution(num_list, n) {
+//     let answer = [];
+    
+//     for(let i=0; i<n; i++) {
+//         answer.push(num_list[i]);
+//     }
+    
+//     return answer;
+    
+    return num_list.slice(0, n);
+}
