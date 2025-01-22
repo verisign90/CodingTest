48563134562ce1671748aabc39106012ff9fa9ce
김재정 <137398992+verisign90@users.noreply.github.com>
2025-01-22 12:22:27 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,5 +1,17 @@
 -- 코드를 입력하세요
+-- select sex_upon_intake
+-- from animal_ins
+-- where sex_upon_intake not in ('Neutered Male', 'Spayed Female');
+
+-- SELECT animal_id, name, 
+--        decode(sex_upon_intake, 'Neutered Male', 'O', 'Spayed Female', 'O', 'X') as 중성화
+-- from animal_ins
+-- order by animal_id;
+
 SELECT animal_id, name,
-       decode(sex_upon_intake, 'Neutered Male', 'O', 'Spayed Female', 'O', 'X') as "중성화"
+    case
+        when sex_upon_intake like 'Intact%' then 'X'
+        else 'O'
+    end as 중성화
 from animal_ins
 order by animal_id;
